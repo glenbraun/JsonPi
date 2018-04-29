5ed9a634dd8c685cdae415748a5357831abd367b
Glen Braun <glen@glenbraun.com>
2018-04-28 17:07:49 -0700

    Cleaned up code of old style command parser.

--- JsonPiREPL/PiRepl.fs
+++ JsonPiREPL/PiRepl.fs
@@ -143,46 +143,6 @@ let Help () =
     """
     Console.WriteLine(msg)
 
-
-let rec Read (buffer, index, count) =
-    if quit then
-        0
-    else 
-        let prompt = 
-            match (multiline, step) with
-            | (false, false) -> "Run> "
-            | (false, true) -> "Step> "
-            | (true, false) -> "Run>> "
-            | (true, true) -> "Step >> "
-    
-        Console.Write(prompt)
-
-        let line = Console.ReadLine().Trim()
-        
-        let line2 = 
-            if line = "go" 
-            then Environment.NewLine
-            else
-                if multiline
-                then 
-                    if (line.StartsWith(":"))
-                    then 
-                        // this is a hack, but it is convienient to have these commands in multiline mode
-                        match line with
-                        | ":h" | ":help" -> Help(); " "
-                        | ":l" | ":list" -> ListPending(); " "
-                        | ":s" | ":step" -> StepMode(); " "
-                        | ":r" | ":run" -> RunMode(); " "
-                        | ":m" | ":multi" -> ToggleMultiline(); Environment.NewLine
-                        | ":reset" -> Reset(); " "
-                        | _ -> Console.WriteLine("Command not supported in multiline mode. Use :m to exit multiline mode."); " "
-                    else line + " " 
-                else line + Environment.NewLine    
-
-        Array.blit (line2.ToCharArray()) 0 buffer index (line2.Length)
-        line2.Length
-
-    
 let rec Run() =
     let prompt = 
         match (multiline, step) with
